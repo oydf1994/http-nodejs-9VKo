2a64f71807a6e7fb6804d4df623fff0830da0232
east <east0616@163.com>
2023-04-09 11:20:43 +0800

    1

--- server.js
+++ server.js
@@ -4,14 +4,17 @@ let api;
 getApi.then(res => {
   api = res
 })
-async function getApi() {
-  // To use ESM in CommonJS, you can use a dynamic import like this:
-  const { ChatGPTAPI } = await import('chatgpt')
-  // You can also try dynamic importing like this:
-  // const importDynamic = new Function('modulePath', 'return import(modulePath)')
-  // const { ChatGPTAPI } = await importDynamic('chatgpt')
-  const api = new ChatGPTAPI({ apiKey: process.env.OPENAI_API_KEY })
-  return api
+function getApi() {
+  return new Promise(async (resolve, reject) => {
+    // To use ESM in CommonJS, you can use a dynamic import like this:
+    const { ChatGPTAPI } = await import('chatgpt')
+    // You can also try dynamic importing like this:
+    // const importDynamic = new Function('modulePath', 'return import(modulePath)')
+    // const { ChatGPTAPI } = await importDynamic('chatgpt')
+    const api = new ChatGPTAPI({ apiKey: process.env.OPENAI_API_KEY })
+    return resolve(api)
+  })
+
 
 }
 app.use(async ctx => {
