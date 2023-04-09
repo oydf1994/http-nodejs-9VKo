c9430e205c26c807c4b30d26f1d3d1be75bd97e2
east <east0616@163.com>
2023-04-09 11:19:10 +0800

    1

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const Koa = require('koa');
 const app = new Koa();
-app.use(ctx => {
-  ctx.body = 'Hello Koa';
+let api;
+getApi.then(res => {
+  api = res
+})
+async function getApi() {
+  // To use ESM in CommonJS, you can use a dynamic import like this:
+  const { ChatGPTAPI } = await import('chatgpt')
+  // You can also try dynamic importing like this:
+  // const importDynamic = new Function('modulePath', 'return import(modulePath)')
+  // const { ChatGPTAPI } = await importDynamic('chatgpt')
+  const api = new ChatGPTAPI({ apiKey: process.env.OPENAI_API_KEY })
+  return api
+
+}
+app.use(async ctx => {
+  const res = await api.sendMessage('Hello World!')
+  console.log(res.text)
+  ctx.body = res.text;
 });
 app.listen(process.env.PORT);
